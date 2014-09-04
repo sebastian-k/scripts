b4e5eb366121276070af5bb8fd0ce16d74c581fd
Sebastian Koenig <sebastiankoenig@posteo.de>
2014-09-04 13:23:57 +0200

    separate reconstruction from solving, rearrange some pies.

--- tracking_pies.py
+++ tracking_pies.py
@@ -16,49 +16,38 @@ import bpy
 from bpy.types import Menu
 
 
-############### FUNCTIONS ##########################
-
-
-
 
 class CLIP_marker_pie(Menu):
-    # label is displayed at the center of the pie menu.
+    # Settings for the individual markers
     bl_label = "Marker Settings"
     bl_idname = "clip.marker_pie"
 
     def draw(self, context):
 
-
         layout = self.layout
         clip = context.space_data.clip
         settings = clip.tracking.settings
+        active = clip.tracking.tracks.active
 
         pie = layout.menu_pie()
-        active = clip.tracking.tracks.active
 
         op = pie.operator("wm.context_set_enum", text="Loc", icon="OUTLINER_DATA_EMPTY")
         op.data_path="space_data.clip.tracking.tracks.active.motion_model"
         op.value="Loc"
-        
         op = pie.operator("wm.context_set_enum", text="Affine", icon="OUTLINER_DATA_LATTICE")
         op.data_path="space_data.clip.tracking.tracks.active.motion_model"
         op.value="Affine"
-
-
 
         if clip.tracking.tracks.active.use_blue_channel:
             pie.prop(active, "use_blue_channel", text="Blue Channel (ON)", icon="FILTER")
         else:
             pie.prop(active, "use_blue_channel", text="Blue Channel (OFF)", icon="FILTER")
-
-        
         pie.operator("clip.track_settings_as_default", icon="SETTINGS")
+
         if clip.tracking.tracks.active.use_normalization:
             pie.prop(active, "use_normalization", text="Normalization (ON)", icon="IMAGE_ALPHA")
         else:
             pie.prop(active, "use_normalization", text="Normalization (OFF", icon="IMAGE_ALPHA")
-
-
         if clip.tracking.tracks.active.use_brute:
             pie.prop(active, "use_brute", text="Prepass (ON)", icon="FILTER")
         else:
@@ -67,7 +56,6 @@ class CLIP_marker_pie(Menu):
         op = pie.operator("wm.context_set_enum", text="Match Previous", icon="KEY_HLT")
         op.data_path="space_data.clip.tracking.tracks.active.pattern_match"
         op.value="PREV_FRAME"
-
         op = pie.operator("wm.context_set_enum", text="Match Keyframe", icon="KEY_HLT")
         op.data_path="space_data.clip.tracking.tracks.active.pattern_match"
         op.value="KEYFRAME"
@@ -77,14 +65,15 @@ class CLIP_marker_pie(Menu):
 
 
 class CLIP_tracking_pie(Menu):
-    # label is displayed at the center of the pie menu.
-    bl_label = "Tracking Settings"
+    # Tracking Operators
+    bl_label = "Tracking"
     bl_idname = "clip.tracking_pie"
 
     def draw(self, context):
         layout = self.layout
 
         pie = layout.menu_pie()
+        
         prop = pie.operator("clip.track_markers", icon="PLAY_REVERSE")
         prop.backwards = True
         prop.sequence = True
@@ -94,15 +83,18 @@ class CLIP_tracking_pie(Menu):
         
         pie.operator("clip.detect_features", icon="ZOOM_SELECTED")
         pie.operator("clip.disable_markers", icon="RESTRICT_VIEW_ON")
+
         pie.operator("clip.clear_track_path", icon="BACK").action="UPTO"
         pie.operator("clip.clear_track_path", icon="FORWARD").action="REMAINED"
+
         pie.operator("clip.refine_markers", icon="LOOP_BACK").backwards=True
         pie.operator("clip.refine_markers", icon="LOOP_FORWARDS").backwards=False
 
 
+
 class CLIP_clipsetup_pie(Menu):
-    # label is displayed at the center of the pie menu.
-    bl_label = "Clip Editor Pie"
+    # Setup the clip display options
+    bl_label = "Clip and Display Setup"
     bl_idname = "clip.clipsetup_pie"
 
     def draw(self, context):
@@ -111,8 +103,10 @@ class CLIP_clipsetup_pie(Menu):
         sc = context.space_data
 
         pie = layout.menu_pie()
+
         pie.operator("clip.reload", text="Reload Footage", icon="FILE_REFRESH")
         pie.operator("clip.prefetch", text="Prefetch Footage", icon="LOOP_FORWARDS")
+        
         pie.prop(sc, "use_mute_footage", text="Mute Footage", icon="MUTE_IPO_ON")
         if sc.clip_user.use_render_undistorted:
             pie.prop(sc.clip_user, "use_render_undistorted", text="Render Undistorted (ON)", icon="MESH_GRID")
@@ -121,6 +115,7 @@ class CLIP_clipsetup_pie(Menu):
 
         pie.prop(sc, "show_names", text="Show Track Info", icon="WORDWRAP_ON")
         pie.prop(sc, "lock_selection", text="Lock", icon="LOCKED")
+        
         pie.prop(sc, "show_disabled", text="Show Disabled Tracks", icon="VISIBLE_IPO_ON")
         pie.prop(sc, "show_marker_search", text="Display Search Area", icon="EDIT_VEC")
 
@@ -128,8 +123,8 @@ class CLIP_clipsetup_pie(Menu):
 
 
 class CLIP_solver_pie(Menu):
-    # label is displayed at the center of the pie menu.
-    bl_label = "Scene Reconstruction"
+    # Operators to solve the scene
+    bl_label = "Solving"
     bl_idname = "clip.solver_pie"
 
     def draw(self, context):
@@ -140,32 +135,24 @@ class CLIP_solver_pie(Menu):
 
         pie = layout.menu_pie()
 
-        # Create a Plane Track
         pie.operator("clip.create_plane_track", icon="MESH_PLANE")
-        # Solve the Camera
         pie.operator("clip.solve_camera", text="Solve Camera", icon="OUTLINER_OB_CAMERA")
 
-        # Clean Tracks 
         op= pie.operator("clip.clean_tracks", icon="STICKY_UVS_DISABLE")
         op.frames=15
         op.error=2
-        # Use Tripod Solving
         if settings.use_tripod_solver:
             pie.prop(settings, "use_tripod_solver", text="Tripod Solver (ON)", icon="RESTRICT_RENDER_OFF")
         else:
             pie.prop(settings, "use_tripod_solver", text="Tripod Solver (OFF)", icon="RESTRICT_RENDER_ON")
 
 
-        # Set Keyframe A
         pie.operator("clip.set_solver_keyframe", text="Set Keyframe A", icon="KEY_HLT").keyframe='KEYFRAME_A'
-        # Set Keyframe B
         pie.operator("clip.set_solver_keyframe", text="Set Keyframe B", icon="KEY_HLT").keyframe='KEYFRAME_B'
 
-        # Use no Refinement
         op = pie.operator("wm.context_set_enum", text="No refinement", icon="CAMERA_DATA")
         op.data_path="space_data.clip.tracking.settings.refine_intrinsics"
         op.value="NONE"
-        # Use full Refinement
         op = pie.operator("wm.context_set_enum", text="Refine Focal Length", icon="CAMERA_DATA")
         op.data_path="space_data.clip.tracking.settings.refine_intrinsics"
         op.value="FOCAL_LENGTH"
@@ -175,23 +162,24 @@ class CLIP_solver_pie(Menu):
 
 class CLIP_reconstruction_pie(Menu):
     # label is displayed at the center of the pie menu.
-    bl_label = "Reconstruction Specials"
-    bl_idname = "clip.reconstruction_specials_pie"
+    bl_label = "Reconstruction"
+    bl_idname = "clip.reconstruction_pie"
 
-    
 
     def draw(self, context):
-
 
         clip = context.space_data.clip
         settings = clip.tracking.settings
         layout = self.layout
 
         pie = layout.menu_pie()
+  
+        pie.operator("clip.set_scale", text="Set Scale", icon="ARROW_LEFTRIGHT")
         pie.operator("clip.setup_tracking_scene", text="Setup Tracking Scene", icon="SCENE_DATA")
-        pie.operator("clip.set_plane", text="Set Floor", icon="MESH_PLANE")
+  
+        pie.operator("clip.set_plane", text="Setup Floor", icon="MESH_PLANE")
         pie.operator("clip.set_origin", text="Set Origin", icon="MANIPUL")
-        pie.operator("clip.set_scale", text="Set Scale", icon="MANIPUL")
+  
         pie.operator("clip.set_axis", text="Set X Axis", icon="AXIS_FRONT").axis="X"
         pie.operator("clip.set_axis", text="Set Y Axis", icon="AXIS_SIDE").axis="Y"
 
@@ -206,12 +194,16 @@ class CLIP_timecontrol_pie(Menu):
         layout = self.layout
 
         pie = layout.menu_pie()
+      
         pie.operator("screen.frame_jump", text="Jump to Startframe", icon="TRIA_LEFT").end=False
         pie.operator("screen.frame_jump", text="Jump to Endframe", icon="TRIA_RIGHT").end=True
+      
         pie.operator("clip.frame_jump", text="Start of Track", icon="REW").position="PATHSTART"
         pie.operator("clip.frame_jump", text="End of Track", icon="FF").position="PATHEND"
-        pie.operator("screen.animation_play", text="Playback Forwards", icon="PLAY").reverse=False
+      
         pie.operator("screen.animation_play", text="Playback Backwards", icon="PLAY_REVERSE").reverse=True
+        pie.operator("screen.animation_play", text="Playback Forwards", icon="PLAY").reverse=False
+      
         pie.operator("screen.frame_offset", text="Previous Frame", icon="TRIA_LEFT").delta=-1
         pie.operator("screen.frame_offset", text="Next Frame", icon="TRIA_RIGHT").delta=1
 
@@ -221,10 +213,6 @@ class CLIP_timecontrol_pie(Menu):
 
 
 ########## register ############
-
-
-
-
 
 
 def register():
@@ -242,11 +230,10 @@ def register():
     kmi = km.keymap_items.new('wm.call_menu_pie', 'W', 'PRESS').properties.name = "clip.clipsetup_pie"
     kmi = km.keymap_items.new('wm.call_menu_pie', 'E', 'PRESS').properties.name = "clip.tracking_pie"
     kmi = km.keymap_items.new('wm.call_menu_pie', 'S', 'PRESS', shift=True).properties.name = "clip.solver_pie"
-    kmi = km.keymap_items.new('wm.call_menu_pie', 'S', 'PRESS', shift=True, alt=True).properties.name = "clip.reconstruction_specials_pie"
+    kmi = km.keymap_items.new('wm.call_menu_pie', 'W', 'PRESS', shift=True).properties.name = "clip.reconstruction_pie"
 
     km = wm.keyconfigs.addon.keymaps.new(name='Frames')
     kmi = km.keymap_items.new('wm.call_menu_pie', 'A', 'PRESS', oskey=True).properties.name = "clip.timecontrol_pie"
-
 
 
 
