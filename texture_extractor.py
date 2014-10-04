1b1870d9210da9ed782c31668bd608b65b72b1de
Sebastian Koenig <sebastiankoenig@posteo.de>
2014-10-04 12:13:20 +0200

    adding a script to automatically create hooks and a mesh for selected track empties, as well as the latest version of the texture extractor

--- texture_extractor.py
+++ texture_extractor.py
@@ -1,47 +1,107 @@
 import bpy
 
 
+# create node setup
+# button for nodes
+# create retouching setup
+# create setup with shadow catching: 2 renderlayers
+# set object with hooks: select hooks, create mesh with vertices near empties
+# for all empties: write position
+# for all vertices: if position is close to empty, --> position == empty, mark as hook
+# for all vertices: create hook
+
+
 
 def prepare_mesh(context, ob, size, canvas, clip):
 
     me = ob.data
+    # see if object is already prepared.das
+    if not "is_prepared" in ob:
+        # set Edit mode if needed
+        if not context.object.mode=="EDIT":
+            bpy.ops.object.mode_set(mode="EDIT")
+            bpy.ops.mesh.select_all(action='SELECT')
 
-    # set Edit mode if needed
-    if not context.object.mode=="EDIT":
-        bpy.ops.object.mode_set(mode="EDIT")
-    # make sure mesh is dense enough for projection
-    if len(me.vertices)<64:
-        bpy.ops.mesh.subdivide(number_cuts=32, smoothness=0)
-    # unwrap and go back to object mode
-    bpy.ops.uv.smart_project(island_margin=0.02)
-    bpy.ops.object.mode_set(mode="OBJECT")
+        # make sure mesh is dense enough for projection
+        if len(me.vertices)<64:
+            sub = ob.modifiers.new(name="Subsurf", type="SUBSURF")
+            sub.subdivision_type="SIMPLE"
+            sub.levels=4
+            sub.render_levels=4
+            bpy.context.object.modifiers["Subsurf"].subdivision_type = 'SIMPLE'
 
-    # make sure we have something to paint on
-    if len(list(me.uv_textures)) > 0:
-        if not me.uv_textures.get("projection"):
+        # unwrap and go back to object mode
+        bpy.ops.uv.smart_project(island_margin=0.02)
+        bpy.ops.object.mode_set(mode="OBJECT")
+
+        # make sure we have something to paint on
+        if len(list(me.uv_textures)) > 0:
+            if not me.uv_textures.get("projection"):
+                me.uv_textures.new(name="projection")
+        else:
+            me.uv_textures.new(name="UVMap")
             me.uv_textures.new(name="projection")
-    else:
-        me.uv_textures.new(name="UVMap")
-        me.uv_textures.new(name="projection")
 
-    # put a canvas onto the mesh for bake and paint
-    for t in  me.uv_textures:
-        if t.active:
-            uvtex = t.data
-            for f in me.polygons:
-                #check that material had an image!
-                uvtex[f.index].image = canvas
+        # put a canvas onto the mesh for bake and paint
+        for t in  me.uv_textures:
+            if t.active:
+                uvtex = t.data
+                for f in me.polygons:
+                    #check that material had an image!
+                    uvtex[f.index].image = canvas
 
-    me.update()
+        me.update()
+        ob["is_prepared"] = True
+        # create the uv-project modifier
+        if not ob.modifiers.get("UVProject"):
+            projector = ob.modifiers.new(name="UVProject", type="UV_PROJECT")
+            projector.uv_layer = "projection"
+            projector.aspect_x=clip.size[0]
+            projector.aspect_y=clip.size[1]
+            projector.projectors[0].object = bpy.data.objects["Camera"]
+            projector.uv_layer="projection"
 
-    # create the uv-project modifier
-    if not ob.modifiers.get("UVProject"):
-        projector = ob.modifiers.new(name="UVProject", type="UV_PROJECT")
-        projector.uv_layer = "projection"
-        projector.aspect_x=clip.size[0]
-        projector.aspect_y=clip.size[1]
-        projector.projectors[0].object = bpy.data.objects["Camera"]
-        projector.uv_layer="projection"
+
+def change_viewport_background_for_painting(context, clip):
+    # prepare the viewport for painting
+    # changing from movie to image will make it updates.
+    space = context.space_data
+
+
+    for img in space.background_images:
+        if img.source == 'MOVIE_CLIP':
+            path = img.clip.filepath
+            length = img.clip.frame_duration
+            start = img.clip.frame_start
+            offset = img.clip.frame_offset
+            if bpy.data.images.get(clip.name):
+                clipimage=bpy.data.images[clip.name]
+            else:
+                clipimage = bpy.data.images.load(path)
+            print(clipimage)
+            
+            img.show_on_foreground = True
+            img.source = "IMAGE"
+            img.image=clipimage
+
+            backimg = img.image_user
+
+            configure_video_image(context, clip, backimg)
+
+
+def set_cleanplate_brush(context, clip, canvas, ob):
+    me = ob.data
+    
+    paint_settings = bpy.context.tool_settings.image_paint
+    ps = paint_settings
+    ps.brush=bpy.data.brushes['Clone']
+    ps.brush.strength=1
+    ps.use_clone_layer=True
+    ps.mode="IMAGE"
+    ps.canvas=canvas
+    ps.clone_image = bpy.data.images[clip.name]
+    ps.use_normal_falloff=False
+    me.uv_texture_clone = me.uv_textures.get("projection")
 
 
 def configure_video_image(context, clip, image):
@@ -123,7 +183,7 @@ def set_baked_mat(context, ob, clip, canvas, movietype):
         mat.use_shadeless=True
 
         mtex=mat.texture_slots.add()
-        mtex.texture = texture
+        mtex.texture = tex
         mtex.texture_coords = 'UV'
         mtex.uv_layer = 'UVMap'
         mtex.use_map_color_diffuse = True
@@ -134,6 +194,9 @@ def set_baked_mat(context, ob, clip, canvas, movietype):
         ob.data.materials.append(mat)
     else:
         ob.material_slots[0].material = mat
+
+
+
 
 ################ CLASSES ##########################
 
@@ -170,16 +233,75 @@ class VIEW3D_texture_extraction_setup(bpy.types.Operator):
 
 
 
+class VIEW3D_cleanplate_painter_setup(bpy.types.Operator):
+    bl_idname = "object.cleanplate_painter_setup"
+    bl_label = "Cleanplate Paint Setup"
+
+    @classmethod
+    def poll(cls, context):
+        space = context.space_data
+        return space.type == 'VIEW_3D'
+
+    def execute(self, context):
+
+        clip = context.scene.active_clip
+        cleaned_object = context.active_object
+
+        clean_name = "cleanplate" + "_" + cleaned_object.name
+        size = 2048
+        movietype = clip.source
+
+        # create a canvas to paint and bake on
+        if not bpy.data.images.get(clean_name):
+            canvas = bpy.data.images.new(clean_name, size, size)
+        else:
+            canvas=bpy.data.images[clean_name]
+
+        prepare_mesh(context, cleaned_object, size, canvas, clip)
+        change_viewport_background_for_painting(context, clip)
+        set_cleanplate_brush(context, clip, canvas, cleaned_object)
+
+        context.space_data.viewport_shade="SOLID"
+        context.space_data.show_textured_solid = True
+    
+        if not context.object.mode=="TEXTURE_PAINT":
+            bpy.ops.object.mode_set(mode="TEXTURE_PAINT")
+        
+        return {'FINISHED'}
+
+
+class VIEW3D_cleanplate_creator(bpy.types.Panel):
+    bl_idname = "object.cleanplate_creator_setup"
+    bl_label = "Cleanplate Creator" 
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "Tools"
+
+    def draw(self, context):
+        layout = self.layout
+
+        col = layout.column(align=True)
+        col.operator("object.texture_extraction_setup")
+        col.operator("object.cleanplate_painter_setup")
+
+
+
+
+
 ########## REGISTER ############
 
 def register():
     bpy.utils.register_class(VIEW3D_texture_extraction_setup)
+    bpy.utils.register_class(VIEW3D_cleanplate_painter_setup)
+    bpy.utils.register_class(VIEW3D_cleanplate_creator)
 
 
 
 def unregister():
 
     bpy.utils.unregister_class(VIEW3D_texture_extraction_setup)
+    bpy.utils.unregister_class(VIEW3D_cleanplate_painter_setup)
+    bpy.utils.unregister_class(VIEW3D_cleanplate_creator)
 
 if __name__ == "__main__":
     register()
